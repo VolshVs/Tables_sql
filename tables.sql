62aecd0ae2394d243befb7e44304463e177caca5
VolshVs <89104047740@mail.ru>
2023-11-20 22:40:26 +0300

    tables.sql changed

--- tables.sql
+++ tables.sql
@@ -1,33 +1,30 @@
-CREATE TABLE IF NOT EXISTS Track (
-	track_id SERIAL PRIMARY KEY,
-	track_title VARCHAR(40) NOT NULL,
-	duration VARCHAR(40) NOT NULL,
-	album VARCHAR(40) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS Album (
 	album_id SERIAL PRIMARY KEY,
-	album_title VARCHAR(40) NOT NULL,
-	year_of_release DATE NOT NULL,
-	artist VARCHAR(40) NOT NULL,
-	track_id INTEGER NOT NULL REFERENCES Track(track_id)
+	album_title VARCHAR(40) UNIQUE NOT NULL,
+	year_of_release INTEGER NOT NULL CHECK (year_of_release between 1900 and 2023)
+);
+
+CREATE TABLE IF NOT EXISTS Track (
+	track_id SERIAL PRIMARY KEY,
+	track_title VARCHAR(40) UNIQUE NOT NULL,
+	duration INTEGER NOT NULL CHECK (duration between 1 and 499280),
+	album_id INTEGER NOT NULL REFERENCES Album(album_id)
 );
 
 CREATE TABLE IF NOT EXISTS Artist (
 	artist_id SERIAL PRIMARY KEY,
-	name_pseudonym VARCHAR(40) NOT NULL,
-	genre VARCHAR(40) NOT NULL
+	name_pseudonym VARCHAR(40) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Genre (
 	genre_id SERIAL PRIMARY KEY,
-	genre_title VARCHAR(40) NOT NULL
+	genre_title VARCHAR(40) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Collection  (
 	collection_id SERIAL PRIMARY KEY,
-	collection_title VARCHAR(40) NOT NULL,
-	year_of_realease DATE NOT NULL,
+	collection_title VARCHAR(40) UNIQUE NOT NULL,
+	year_of_realease INTEGER NOT NULL CHECK (year_of_realease between 1900 and 2023),
 	tracks VARCHAR(40) NOT NULL
 );
 
